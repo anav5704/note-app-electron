fd13cc470ef6f11f9df4ee5f84983573afa882a1
anav5704 <anavkki@gmail.com>
2024-01-25 09:53:55 +1200

    build: configure typescript

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,18 +3,36 @@ import { defineConfig, externalizeDepsPlugin } from "electron-vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  main: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  renderer: {
+    main: {
+        plugins: [externalizeDepsPlugin()],
+        resolve: {
+            alias: {
+                "@/lib": resolve("src/main/lib"),
+                "@shared": resolve("src/shared"),   
+            },
+        },
+    },
+    preload: {
+    plugins: [externalizeDepsPlugin()],
+    assetsInclude: "src/renderer/assets/**",
+      resolve: {
+            alias: {
+                "@/renderer": resolve("src/renderer/src"),
+                "@shared": resolve("src/shared"),
+                '@/hooks': resolve('src/renderer/src/hooks'),
+                '@/assets': resolve('src/renderer/src/assets'),
+                '@/store': resolve('src/renderer/src/store'),
+                '@/components': resolve('src/renderer/src/components'),
+                '@/mocks': resolve('src/renderer/src/mocks')
+            },
+        },
+},
+    renderer: {
     resolve: {
-      alias: {
-        "@renderer": resolve("src/renderer/src")
-      }
+        alias: {
+            "@renderer": resolve("src/renderer/src")
+        }
     },
     plugins: [react()]
-  }
+}
 })
