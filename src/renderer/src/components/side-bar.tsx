37a2678cdbfab4a2727b051c7ea75d22adadb0cb
anav5704 <anavkki@gmail.com>
2024-01-28 09:50:55 +1200

    feat: create and delelte notes

--- src/renderer/src/components/side-bar.tsx
+++ src/renderer/src/components/side-bar.tsx
@@ -1,6 +1,8 @@
 import { FilePlus2, Plus, Trash2 } from 'lucide-react'
 import { cn } from '@renderer/utils'
 import { useNoteList } from '@renderer/hooks/useNoteList'
+import { useSetAtom } from 'jotai'
+import { createEmptyNoteAtom, deleteNoteAtom } from '@renderer/store'
 
 interface SideBarProps {
     className?: string,
@@ -8,12 +10,22 @@ interface SideBarProps {
 }
 
 export const SideBar = ({ className, onSelect }: SideBarProps) => {
-    const { notes, selectedNoteIndex, handleNoteSelect} = useNoteList({ onSelect })
+    const { notes, selectedNoteIndex, handleNoteSelect } = useNoteList({ onSelect })
+    const createEmptyNote = useSetAtom(createEmptyNoteAtom)
+    const deleteNote = useSetAtom(deleteNoteAtom)
+
+    const handleCreate = () => {
+        createEmptyNote()
+    }
+
+    const handleDelete = () => {
+        deleteNote()
+    }
 
     return (
         <aside className={cn("w-[200px] border-r border-zinc-800 ", className)}>
             <div className='text-xs group border-b last-of-type:border-none border-zinc-800'>
-                <div className='cursor-pointer p-4 grid place-content-center w-full'>
+                <div onClick={handleCreate} className='cursor-pointer p-4 grid place-content-center w-full'>
                     <Plus className='h-4 w-4 text-zinc-600' />
                 </div>
             </div>
@@ -23,7 +35,7 @@ export const SideBar = ({ className, onSelect }: SideBarProps) => {
                         <p className='line-clamp-1'>{note.title}</p>
                         <p className='opacity-25 font-light'>{note.updatedAt}</p>
                     </div>
-                    <div className='transition cursor-pointer p-2 opacity-0 group-hover:opacity-100'>
+                    <div onClick={handleDelete} className='transition cursor-pointer p-2 opacity-0 group-hover:opacity-100'>
                         <Trash2 className='h-4 w-4 text-zinc-600' />
                     </div>
                 </div>
--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -1,19 +1,17 @@
 import { Note } from "@shared/models/note"
 import { atom } from "jotai"
 
-
-
 const mockData = [
-  {
-    title: "Mahoraga Stats",
-    content: "Content note 1",
-    updatedAt: new Date().getTime()
-  },
-  {
-    title: "Sukuna Stats",
-    content: "Content note 2",
-    updatedAt: new Date().getTime()
-  }
+    {
+        title: "Mahoraga Stats",
+        content: "Content note 1",
+        updatedAt: new Date().getTime()
+    },
+    {
+        title: "Sukuna Stats",
+        content: "Content note 2",
+        updatedAt: new Date().getTime()
+    }
 ]
 
 export const notesAtom = atom<Note[]>(mockData)
@@ -21,12 +19,39 @@ export const notesAtom = atom<Note[]>(mockData)
 export const selectedNoteIndexAtom = atom<number | null>(null)
 
 export const selectedNoteAtom = atom((get) => {
-  const notes = get(notesAtom)
-  const selectedNoteIndex = get(selectedNoteIndexAtom)
+    const notes = get(notesAtom)
+    const selectedNoteIndex = get(selectedNoteIndexAtom)
 
-  if (selectedNoteIndex === null) return null
+    if (selectedNoteIndex === null) return null
 
-  const selectedNote = notes[selectedNoteIndex]
+    const selectedNote = notes[selectedNoteIndex]
 
-  return { ...selectedNote, content: `Hello from note ${selectedNoteIndex}` }
+    return { ...selectedNote, content: `Hello from note ${selectedNoteIndex}` }
+})
+
+export const createEmptyNoteAtom = atom(null, (get, set) => {
+    const notes = get(notesAtom)
+    const title = `Title for note ${notes.length + 1}`
+
+    const newNote: Note = {
+        title,
+        content: "",
+        updatedAt: Date.now()
+    }
+
+    set(notesAtom, [newNote, ...notes.filter((note) => note.title !== newNote.title)])
+    set(selectedNoteIndexAtom, 0)
+})
+
+export const deleteNoteAtom = atom(null, (get, set) => {
+    const notes = get(notesAtom)
+    const selectedNote = get(selectedNoteAtom)
+
+    if (!selectedNote) return
+
+    set(
+        notesAtom,
+        notes.filter((note) => note.title !== selectedNote.title)
+    )
+    set(selectedNoteIndexAtom, null)
 })
